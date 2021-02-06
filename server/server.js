9f3ea2ff03fef92024c5a7b7de25d0cd9dcd47c8
Benntobox <benjaminsboyle3@gmail.com>
2021-02-06 14:30:25 -0800

    Fixed routing, added front end styling and images

--- server/server.js
+++ server/server.js
@@ -10,18 +10,13 @@ app.use(cors())
 
 app.get('/amenities-api/amenities', function (req, res) {
   retrieve({})
-    .then(data => {
-      if (data.length === 0) { res.status(400).json("ID not found") }
-      else { res.json(data); }
-    })
+    .then(data => res.json(data));
 });
 
 app.get('/amenities-api/amenity/:id', function (req, res) {
-  retrieve({ id: req.params.id })
-    .then(data => {
-      console.log(data)
-      data.length === 0 ? res.status(400).json("ID not found") : res.json(data[0]);
-    });
+  const { id } = req.params;
+  retrieve({ id })
+    .then(data => res.json(data[0]));
 });
 
 app.get('/favicon.ico', (req, res) => res.end());
