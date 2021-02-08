e1e2cc2067e58f9c054713c0e6d3c9e6acc7d534
Benntobox <benjaminsboyle3@gmail.com>
2021-02-08 09:48:12 -0800

    Minor fixes to server fallback port and retrieve base case

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const { retrieve } = require('./db/index.js')
-const cors = require('cors')
+const { retrieve } = require('./db/index.js');
+const cors = require('cors');
 const bodyParser = require ('body-parser');
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(express.static(__dirname + '/../client/dist'));
-app.use(cors())
+app.use(cors());
 
 app.get('/amenities-api/amenities', function (req, res) {
   retrieve({})
@@ -16,7 +16,9 @@ app.get('/amenities-api/amenities', function (req, res) {
 app.get('/amenities-api/amenity/:id', function (req, res) {
   const { id } = req.params;
   retrieve({ id })
-    .then(data => res.json(data[0]));
+    .then(data =>  {
+      data.length > 0 ? res.json(data[0]) : res.json(data);
+    });
 });
 
 app.get('/favicon.ico', (req, res) => res.end());
